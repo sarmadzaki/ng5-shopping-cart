42d1ce4c66dabd86eeb65b134b855fa65644230e
sarmadzaki <sarmad.zaki@yahoo.com>
2017-11-14 11:51:48 -0800

    styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ApplicationComponent } from './components/application/application.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,11 +12,15 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ProductService } from './services/productService/product.service'
-import { CartService } from './services/cartService/cart.service'
+import { CartService } from './services/cartService/cart.service';
+import { CheckoutComponent } from './components/checkout/checkout.component'
+
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
-}
-
+} 
+const appRoutes: Routes = [ {path: '', component: AppComponent},
+                            {path: 'checkout', component: CheckoutComponent}      
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,9 +28,13 @@ export function HttpLoaderFactory(http: HttpClient) {
     NavbarComponent,
     ProductComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    CheckoutComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule,
     HttpClientModule,
     TranslateModule.forRoot({
